1b9fa7218a40f79efd7d54e6f561ae8c2fbaa38e
Hifza Maqsood <hifzamaqsood_1451@yahoo.com>
2020-06-07 17:08:00 +0500

    JS practice work

---
+++ app.js
@@ -0,0 +1,291 @@
+// chapter #01
+
+// alert('Welcome to a world classy Site');
+
+// alert('Error! Please enter a valid password.');
+
+// alert('Welcome to the JS land ... \n   Happy Coding!');
+
+// for (var i = 0; i <= 1; i++) {
+//     if(i === 0)
+//     alert("Welcome to JS Land... ");
+
+//     else{
+//         alert("Prevent this page from creating this dialogs");
+//     }
+
+// }
+
+
+// chapter #02
+
+
+// 01
+// var userName;
+
+// 02
+// var myName;
+// var str= "Hifza Maqsood Ahmed";
+// myName=str;
+// document.write(myName);
+
+// 03
+// var message="Hello World";
+// alert(message);
+
+// 04
+// var name,age,courseCertified;
+// name= "John Doe";
+// age= "15 years old";
+// courseCertified= "Certified Mobile application Development";
+// alert(name);
+// alert(age);
+// alert(courseCertified);
+
+// 05
+// var fav= "PIZZA";
+// alert(fav +"\n"+ fav[0]+ fav[1]+ fav[2]+ fav[3] + "\n" + fav[0]+ fav[1]+ fav[2] +  "\n" + fav[0]+ fav[1] + "\n" + fav[0] );
+
+
+// 06
+// var email= "example@example.com";
+// alert("My email addresss is " +  email);
+
+// 07
+// var book= "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+
+// 08
+// document.write("Yah! I can write HTML content through JavaScript");
+
+
+// 09
+
+
+
+
+
+// chapter #03
+
+// 01
+// var age= "I am 15 years old";
+// alert(age);
+
+
+// 02
+
+
+
+
+// 03
+// var birthYear= 1990;
+// document.write(" My birthYear is " + birthYear + "<br> "  + "Data type of my declared variable is number" );
+
+
+// 04
+// var visitorName= "John Doe";
+// var proTitle= "T-shirt(s)";
+// var quantity= 5;
+// document.write(visitorName + "  ordered  " + proTitle + " on XYZ Clothing Store");
+
+
+// chapter #04
+
+// 01
+// var name,age,gender;
+
+// 02
+// 5 Legal Variables Names
+// var name;
+// var firstName;
+// var full_name;
+// var name2;
+// var $name;
+
+// 5 Illegal Variables Names
+// var _name;
+// var first name;
+// var full-name;
+// var 1name;
+// var &name;
+
+// 03 done in html file
+
+// chapter #05
+
+// 01
+// var num1= 3;
+// var num2= 5;
+// var sum= num1 + num2;
+// document.write("sum of 3 and 5 is " + sum);
+
+// 02
+// var num1= 5;
+// var num2= 3;
+// var sub= num1 - num2;
+// document.write("Subtraction of 5 and 3 is " + sub);
+
+// var num1= 5;
+// var num2= 3;
+// var mul= num1 * num2;
+// document.write("Multiplication of 5 and 3 is " + mul);
+
+// var num1= 5;
+// var num2= 3;
+// var div= num1 / num2;
+// document.write("Division of 5 and 3 is " + div);
+
+// var num1= 5;
+// var num2= 3;
+// var mod= num1 % num2;
+// document.write("Division of 5 and 3 is " + mod);
+
+// 03
+// var num;
+// document.write("Value after variable declaration is " + num + "<br>");
+// num = 5;
+// document.write("Initial value is: " + num + "<br>");
+// num++;
+// document.write( "Value after increment is: " + num + "<br>");
+// num = num +7;
+// document.write("Value after addition is: " + num + "<br>");
+// num--;
+// document.write("Value after decrement is: " + num + "<br>");
+// num= num % 3;
+// document.write( "The remainder is: " + num + "<br>");
+
+
+// 04
+// var cost= 600;
+// cost= cost * 5;
+// document.write("Total cost to buy 5 tickets to a movie is  " + cost + "PKR" );
+
+// 05
+// var num= 6;
+// for(var i=1; i<=10; i++){
+//   document.write("6" + "X" + i + "=" + num * i + "<br>");
+// }
+
+// 06 Celsius to Fahrenheit
+
+// var C= 25; 
+// var F= 70;
+// F= (25 * 9/5)  + 32;
+// document.write("25" + "<sup> 0 </sup> C" + " is " + F +  "<sup> 0 </sup> F");
+
+//  Fahrenheit to Celsius
+// var C= 27; 
+// var F= 70;
+// C= (70- 32) * 5/9 ;
+// document.write("70" + "<sup> 0 </sup> F" + " is " + C +  "<sup> 0 </sup> C");
+
+// 07
+// var prz1stItem= 650;
+// var prz2ndItem= 100;
+// var firstItemQty= 3;
+// var secondItemQty= 7;
+// var shipChrgs= 100;
+// var total;
+// document.write("<h1>Shopping Cart</h1>");
+// document.write("Price of item 1 is " + prz1stItem + "<br>");
+// document.write("Quantity of item 1 is " + firstItemQty + "<br>");
+// document.write("Price of item 2 is " + prz2ndItem + "<br>");
+// document.write("Quantity of item 2 is " + secondItemQty  + "<br>");
+// document.write("Quantity of item 2 is " + secondItemQty  + "<br>");
+// document.write("Shipping Charges s" + shipChrgs + "<br> <br>");
+// total= prz1stItem * firstItemQty + prz2ndItem * secondItemQty +shipChrgs;
+// document.write("Total cost of your order is  " + total);
+
+// 08
+// var totalMarks= 980;
+// var obtained= 804;
+// var percentage= obtained/totalMarks * 100;
+// document.write("<h1>Marks Sheet </h1>"  + "<br>");
+// document.write("Total Marks: " + totalMarks + "<br>");
+// document.write("Marks obtained: " + obtained + "<br>");
+// document.write("Percentage: " + percentage + "%" );
+
+// 09
+// var $US= 104.80;
+// var saudiRayal= 28;
+// var pkr;
+// pkr= $US * 10 + saudiRayal * 25;
+// document.write("<h1>Currency in PKR</h1>  " +"<br>" );
+// document.write("Total Currency in PKR:" + pkr );
+
+// 10      something wrong
+// var num= 10;
+// num= num + (5 * 10) / 2;
+// document.write(num );
+
+
+// 07
+var currntYear= 2016;
+var birthYear= 1992;
+var age;
+document.write("<h1>Age Calculator </h1>" + "<br>" );
+document.write("Current Year: " + currntYear + "<br>" );
+document.write(" Birth Year: " + birthYear + "<br>" );
+age = currntYear - birthYear;
+document.write("Your age is: " + age );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
